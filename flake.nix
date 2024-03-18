8ba4ebe0a28b98d1c1c2aa93062f73c1748ad73c
Gustavo "Guz" L. de Mello <contact.guz013@gmail.com>
2024-03-18 13:39:50 -0300

    initial commit

---
+++ flake.nix
@@ -0,0 +1,30 @@
+{
+  description = "learning.rs";
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+  outputs = { self, nixpkgs, flake-utils, rust-overlay }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        overlays = [ (import rust-overlay) ];
+        pkgs = import nixpkgs {
+          inherit system overlays;
+        };
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            openssl
+            pkg-config
+            eza
+            fd
+            (rust-bin.stable.latest.default.override {
+              extensions = [ "rust-src" ];
+            })
+            rust-analyzer
+          ];
+        };
+      });
+}
